fca0fa4cf2a0afadc2ad501344bdb1164c4232c3
Linguxa <57569412+Linguxa@users.noreply.github.com>
2019-11-09 19:28:50 +0200

    Update Bloodstain_Angle_Of_Impact.py

--- Bloodstain_Angle_Of_Impact.py
+++ Bloodstain_Angle_Of_Impact.py
@@ -2,7 +2,7 @@ import math
 
 Bloodstain_Width = float(input("Input the width of the bloodstain: "))
 Bloodstain_Length = float(input("Input the length of the bloodstain: "))
-Bloodstain_Angle_Of_Impact = math.asin(Bloodstain_Width/Bloodstain_Length)
-print(Bloodstain_Angle_Of_Impact*57.29578, "Degrees")
+Bloodstain_Angle_Of_Impact = math.asin(Bloodstain_Width/Bloodstain_Length)*57.29578
+print(Bloodstain_Angle_Of_Impact, "Degrees")
 
 
